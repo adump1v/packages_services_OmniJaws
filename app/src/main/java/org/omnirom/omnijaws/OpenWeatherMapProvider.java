deeb26855e3330714227fee47f0cc78bf27ced36
Rafiester <rafi.haqqi@gmail.com>
2024-02-24 10:26:01 +0000

    OmniJaws: Adapt for DerpFest
    Signed-off-by: Rafiester <rafi.haqqi@gmail.com>

--- app/src/main/java/org/omnirom/omnijaws/OpenWeatherMapProvider.java
+++ app/src/main/java/org/omnirom/omnijaws/OpenWeatherMapProvider.java
@@ -44,7 +44,7 @@ public class OpenWeatherMapProvider extends AbstractWeatherProvider {
     private List<String> mKeys = new ArrayList<String>();
     private boolean mHasAPIKey;
     private int mRequestNumber;
-    private static final String DEVICE = "ro.superior.device";
+    private static final String DEVICE = "ro.derpfest.device";
 
     public OpenWeatherMapProvider(Context context) {
         super(context);
--- app/src/main/java/org/omnirom/omnijaws/WeatherContentProvider.java
+++ app/src/main/java/org/omnirom/omnijaws/WeatherContentProvider.java
@@ -15,7 +15,7 @@
  */
 package org.omnirom.omnijaws;
 
-import com.android.internal.util.superior.OmniJawsClient;
+import com.android.internal.util.derp.OmniJawsClient;
 
 import org.omnirom.omnijaws.WeatherInfo.DayForecast;
 
